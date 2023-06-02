5b10f0a52cf525d6ab1ca7f037dc760b361c8674
ugulavaleri <leri.ugulava1@gmail.com>
2023-06-02 20:10:59 +0400

    fix styles

--- src/components/WallOfShame-Scss/wallOfShame-table.jsx
+++ src/components/WallOfShame-Scss/wallOfShame-table.jsx
@@ -3,20 +3,22 @@ import WallOfShameTable_body from "./wallOfShame-body";
 
 const WallOfShameTable_head = ({ users, setUsersArray }) => {
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Excuse</th>
-                    <th>Count</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <WallOfShameTable_body
-                users={users}
-                setUsersArray={setUsersArray}
-            />
-        </table>
+        <div className="Cont">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Excuse</th>
+                        <th>Count</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <WallOfShameTable_body
+                    users={users}
+                    setUsersArray={setUsersArray}
+                />
+            </table>
+        </div>
     );
 };
 
